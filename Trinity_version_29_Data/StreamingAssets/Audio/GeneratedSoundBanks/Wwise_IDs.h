636bf0c295133122df3edf84314cacfdb91a7fdf
patricioGarzon <nicox_g@hotmail.es>
2024-01-16 18:10:13 -0800

    Trinity files executable

---
+++ Trinity_version_29_Data/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,123 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID LOWHEALTH = 1017222595U;
+        static const AkUniqueID PLAY_AIMING = 1286977679U;
+        static const AkUniqueID PLAY_AMB = 2959533290U;
+        static const AkUniqueID PLAY_BLUEBASEALLMOSTDESTROYEDOK_01 = 1389229848U;
+        static const AkUniqueID PLAY_BLUEBASEUNDERATTACKOK_01 = 318055135U;
+        static const AkUniqueID PLAY_BLUETEAMDRONESOK_01 = 125091212U;
+        static const AkUniqueID PLAY_BLUETEAMVICTORYOK_01 = 1648486139U;
+        static const AkUniqueID PLAY_BULLETIMPACT = 140749424U;
+        static const AkUniqueID PLAY_BULLETIMPACTBODY = 2552117268U;
+        static const AkUniqueID PLAY_BULLETIMPACTENEMY = 4217989764U;
+        static const AkUniqueID PLAY_BULLETIMPACTSHIELD = 2842972339U;
+        static const AkUniqueID PLAY_BULLETIMPACTWALL = 3136062220U;
+        static const AkUniqueID PLAY_CAPTUREOBJECTIVESTODEPLOYDRONESOK_03 = 3544605487U;
+        static const AkUniqueID PLAY_CAPTUREPOINTACTIVATEDOK_01 = 2781525929U;
+        static const AkUniqueID PLAY_CAPTUREPOINTCHANGECONTROL = 1058032625U;
+        static const AkUniqueID PLAY_CAPTUREPOINTENTRY = 612954406U;
+        static const AkUniqueID PLAY_DEATHSOUND = 1523662489U;
+        static const AkUniqueID PLAY_DOUBLEKILLOK = 4128609527U;
+        static const AkUniqueID PLAY_FOOTSTEPS = 3854155799U;
+        static const AkUniqueID PLAY_MX_TNTY_UI_MENU = 989083242U;
+        static const AkUniqueID PLAY_REDBASEALLMOSTDESTROYEDOK_03 = 226463801U;
+        static const AkUniqueID PLAY_REDBASUNDERATTACK1 = 2545256020U;
+        static const AkUniqueID PLAY_REDTEAMDRONESOK_01 = 3958441767U;
+        static const AkUniqueID PLAY_REDTEAMVICTORYOK_02 = 511764217U;
+        static const AkUniqueID PLAY_RIFLE = 1920272688U;
+        static const AkUniqueID PLAY_SFX_ABILITY_BEAM = 2441941150U;
+        static const AkUniqueID PLAY_SFX_TNITY_OVERHEAT = 3817237199U;
+        static const AkUniqueID PLAY_SFX_TNTY_ORB_PICKUP = 2146450754U;
+        static const AkUniqueID PLAY_SFX_TNTY_STUNACTIVATED = 2004900905U;
+        static const AkUniqueID PLAY_SFX_TP = 2250680610U;
+        static const AkUniqueID PLAY_SFX_TRINITY_GUNSHOT = 2984203512U;
+        static const AkUniqueID PLAY_SFX_TRINITY_SHIELD = 1820418589U;
+        static const AkUniqueID PLAY_SFX_TRINITY_UI_CONFIRM = 2618511193U;
+        static const AkUniqueID PLAY_SFX_TRINITY_UI_SCROLL = 3478616360U;
+        static const AkUniqueID PLAY_SFX_TRINITY_WEAPONOPERATIVE = 2340494341U;
+        static const AkUniqueID PLAY_SNIPER = 3849153871U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace FOOTSTEPS
+        {
+            static const AkUniqueID GROUP = 2385628198U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID GRASS = 4248645337U;
+            } // namespace SWITCH
+        } // namespace FOOTSTEPS
+
+        namespace HABILITIES
+        {
+            static const AkUniqueID GROUP = 2157941935U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID ACTIVATE_BEAM = 1900435144U;
+                static const AkUniqueID ACTIVATE_STUN = 1062922193U;
+                static const AkUniqueID SHIELD = 1161967626U;
+                static const AkUniqueID TELEPORT = 530129416U;
+            } // namespace SWITCH
+        } // namespace HABILITIES
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID AMB = 1117531639U;
+        static const AkUniqueID FOOTSTEPS = 2385628198U;
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID TRINITY_SOUNDBANKS = 1526997315U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID ENVIROMENT = 3909959462U;
+        static const AkUniqueID GUNSHOTS = 3043617976U;
+        static const AkUniqueID HABILITIES = 2157941935U;
+        static const AkUniqueID HDR = 931844945U;
+        static const AkUniqueID MAIN_CHARACTER = 3094055254U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+        static const AkUniqueID MOVEMENT = 2129636626U;
+        static const AkUniqueID REVERB = 348963605U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID LVL1 = 141331490U;
+    } // namespace AUX_BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
